07dbbb2c67c001be969a36232db2d970c0d32b38
hapoom2 <sje5149@naver.com>
2026-04-01 12:05:51 +0900

    Title: 피보나치 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260401/피보나치 수/fibonacci-number.java
+++ 260401/피보나치 수/fibonacci-number.java
@@ -5,10 +5,14 @@ public class Main {
         int n = sc.nextInt();
         // Please write your code here.
         int dp[] = new int[n+1];
-        dp[1] = 1;
-        dp[2] = 1;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+
+        for(int i=1;i<=n;i++){
+            if(i==1||i==2){
+                dp[i]=1;
+            }else{
+                dp[i] = dp[i-1]+dp[i-2];
+            }
+            
         }
 
         System.out.println(dp[n]);
