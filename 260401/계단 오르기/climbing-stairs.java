c06e70f1c9f8600d9daab72b043d0e93a9f431bf
hapoom2 <sje5149@naver.com>
2026-04-01 12:18:43 +0900

    Title: 계단 오르기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260401/계단 오르기/climbing-stairs.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        int dp[] = new int[n+1];
+        dp[1] = 0;
+        dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            if(i==3){
+                dp[3] = 1;
+            }else{
+                dp[i] = (dp[i-2]==0?0:dp[i-2])+(dp[i-3]==0?0:dp[i-3]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
